149711d924fff44bd753c1d1a374cecd82bdc52d
artandifjrl <69045656+artandifjrl@users.noreply.github.com>
2020-07-31 21:17:29 +0700

    upload web portfolio
    upload web portfolio artandifjr

---
+++ js/script.js.js
@@ -0,0 +1,11 @@
+const txtElement = ['Freelancer', 'Web Designer', 'Graphic Designer', 'UI Designer'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+
+console.log(txtElement[1]);
+
+})();
